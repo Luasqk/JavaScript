1dbccf71a7fad316338c66f53dea3f0569e55566
Lucas Brandao <palarobrandaolucas@edu.unifil.br>
2026-06-18 16:45:08 -0300

    muitos..

---
+++ css-atividades/css/js/tag.js
@@ -0,0 +1,40 @@
+const colors = {
+    p: '#388e3c',
+    div: '#1565c0',
+    span: '#e53935',
+    section: '#f67809',
+    ul: '#5e35b1',
+    ol: '#fbc02d',
+    header: '#d81b60',
+    nav: '#64dd17',
+    main: '#00acc1',
+    footer: '#304ffe',
+    form: '#9f6581',
+    body: '#25b6da',
+    padrao: '#616161',
+    get(tag) {
+        return this[tag] ?this[tag] : this.padrao
+    }
+}
+
+
+
+
+document.querySelectorAll('.tag').forEach(elemento => {
+    const tagName = elemento.tagName.toLowerCase()
+
+    elemento.style.borderStyle = 'solid'
+    elemento.style.borderWidth = '2px' 
+    elemento.style.borderColor = colors.get(tagName)
+
+    if(!elemento.classList.contains('nolabel')) {
+        const label = document.createElement('label')
+        //cria a tag label
+        label.style.backgroundColor = colors.get(tagName)
+        label.innerHTML = tagName
+        //insere cor de fundo e escreve dentro da label o
+        //nome da tag
+        elemento.insertBefore(label, elemento.childNodes[0])
+        //coloca esse label no comeco do elemento    
+    }
+})
